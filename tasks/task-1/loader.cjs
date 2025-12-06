7870ef784e386972ecd5816b5df42af0e645c5da
Vekesser Vladimir <Vladimir Vekesser>
2025-12-06 20:46:58 +0400

    Task 1

--- tasks/task-1/loader.cjs
+++ tasks/task-1/loader.cjs
@@ -1,2 +1,15 @@
 const yaml = require("yaml");
 const fs = require("node:fs");
+
+const loader = function (module, filename) {
+    const content = fs.readFileSync(filename, 'utf8')
+    try {
+        module.exports = yaml.parse(content)
+    } catch (err) {
+        err.message = filename + ': ' + err.message
+        throw err
+    }
+}
+
+require.extensions['.yaml'] = loader;
+require.extensions['.yml'] = loader;
--- tasks/task-1/loader.mjs
+++ tasks/task-1/loader.mjs
@@ -1,3 +1,23 @@
-import fs from "node:fs/promises";
+import fs from "node:fs";
 import { fileURLToPath } from "node:url";
+import path from "node:path";
 import yaml from "yaml";
+
+export async function load(url, context, defaultLoad) {
+  const format = context.format;
+
+  console.debug(url);
+  console.debug(context);
+  console.debug(fileURLToPath(url));
+  if (url.startWith('file://')) {
+    const filePath = fileURLToPath(url);
+    const fileExtName = path.extname(filePath);
+    let fileSource = fs.readFileSync(filePath, "utf8");
+  
+    if (fileExtName === "yaml" || fileExtName === "yml") {
+      console.debug(yaml.parse(fileSource));
+    }
+  }
+
+  return { format, source: fileSource, shortCircuit: true };
+}
