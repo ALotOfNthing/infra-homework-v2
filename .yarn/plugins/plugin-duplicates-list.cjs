f59b2b120f3c278bc8f7177a75af4e9fab7ddf4d
Vekesser Vladimir <Vladimir Vekesser>
2025-12-20 22:52:35 +0400

    Task 2

--- .yarn/plugins/plugin-duplicates-list.cjs
+++ .yarn/plugins/plugin-duplicates-list.cjs
@@ -3,5 +3,19 @@ module.exports = {
   factory: (require) => {
     const { BaseCommand } = require(`@yarnpkg/cli`);
     const { Configuration, Project, structUtils } = require("@yarnpkg/core");
+
+    class DuplicatesCommand extends BaseCommand {
+      static paths = [[`duplicates`]];
+
+      async execute() {
+        this.context.stdout.write(`This is my very own plugin 😎\n`);
+      }
+    }
+
+    return {
+      commands: [
+        DuplicatesCommand,
+      ],
+    };
   },
 };
